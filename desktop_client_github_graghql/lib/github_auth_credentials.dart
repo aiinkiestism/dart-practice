89a5945fa3ad26da074e24b355cbc4f62f81b043
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2020-08-23 05:29:16 +0900

    add failed step4 code

---
+++ desktop_client_github_graghql/lib/github_auth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'f58785f24c2698e02178';
+const githubClientSecret = '9a98949945eaa01185b860eca5bff8e8d094f8dc';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
